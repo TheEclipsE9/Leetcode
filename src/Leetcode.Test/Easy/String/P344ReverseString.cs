a1c6450b0e754f1c3343b084142bd0629c63f476
TheEclipsE <TheEclipsE>
2024-03-18 20:41:46 +0300

    334 Reverse String

---
+++ src/Leetcode.Test/Easy/String/P344ReverseString.cs
@@ -0,0 +1,29 @@
+﻿using FluentAssertions;
+
+namespace Leetcode.Test.Easy.String
+{
+    public class P344ReverseString
+    {
+        [Theory]
+        [InlineData(new char[] { 'h', 'e', 'l', 'l', 'o' }, new char[] { 'o', 'l', 'l', 'e', 'h' })]
+        public void Test(char[] s, char[] expected)
+        {
+            ReverseString(s);
+
+            s.Should().Equal(expected);
+        }
+
+        private void ReverseString(char[] s)
+        {
+            int left = 0;
+            int rigth = s.Length - 1;
+
+            while (left < rigth)
+            {
+                (s[left], s[rigth]) = (s[rigth], s[left]);
+                left++;
+                rigth--;
+            }
+        }
+    }
+}
