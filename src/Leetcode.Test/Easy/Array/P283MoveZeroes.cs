b8d8cf83994c13c8a5c86cdd07ff7f1e8d86fdd6
TheEclipsE <TheEclipsE>
2024-03-19 11:34:14 +0300

    283. Move Zeroes

---
+++ src/Leetcode.Test/Easy/Array/P283MoveZeroes.cs
@@ -0,0 +1,47 @@
+﻿using FluentAssertions;
+
+namespace Leetcode.Test.Easy.Array
+{
+    public class P283MoveZeroes
+    {
+        [Theory]
+        [InlineData(new int[] { 0, 1, 0, 3, 12 }, new int[] { 1, 3, 12, 0, 0 })]
+        [InlineData(new int[] { 1, 0, 0, 3, 12 }, new int[] { 1, 3, 12, 0, 0 })]
+        [InlineData(new int[] { 1, 3, 12, 0, 0 }, new int[] { 1, 3, 12, 0, 0 })]
+        [InlineData(new int[] { 0}, new int[] { 0 })]
+        public void Test(int[] array, int[] expected)
+        {
+            MoveZeroes(array);
+
+            array.Should().Equal(expected);
+        }
+
+        private void MoveZeroes(int[] nums)
+        {
+            int left = 0;
+            int right = 0;
+
+            while (right < nums.Length)
+            {
+                if (nums[left] != 0)
+                {
+                    left++;
+                    right = left;
+                    continue;
+                }
+
+                if (nums[right] == 0)
+                {
+                    right++;
+                    continue;
+                }
+
+
+                (nums[left], nums[right]) = (nums[right], nums[left]);
+
+                left++;
+                right++;
+            }
+        }
+    }
+}
