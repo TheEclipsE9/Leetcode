24371edc1f3bba46740b15c520c9d0c068c7cd14
TheEclipsE <TheEclipsE>
2024-03-19 10:49:01 +0300

    Find Second Maximum value

---
+++ src/Leetcode.Test/NotLeetcode/Array/SecondLargestNumber.cs
@@ -0,0 +1,39 @@
+﻿using FluentAssertions;
+
+namespace Leetcode.Test.NotLeetcode.Array
+{
+    public class SecondLargestNumber
+    {
+        [Theory]
+        [InlineData(new int[] { 4, 5, 6, 7, 0, 1, 2 }, 6)]
+        [InlineData(new int[] { 13, 15, 17, 11 }, 15)]
+        [InlineData(new int[] { 5, 9, 3, 15, 1, 2 }, 9)]
+        [InlineData(new int[] { 15, 9, 3, 15, 1, 2 }, 9)]
+        [InlineData(new int[] { 15, 9 }, 9)]
+        public void Test(int[] array, int expected)
+        {
+            FindSecondLargest(array).Should().Be(expected);
+        }
+
+        private int FindSecondLargest(int[] array)
+        {
+            int max = int.MinValue;
+            int secondMax = int.MinValue;
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i] > max)
+                {
+                    secondMax = max;
+                    max = array[i];
+                }
+                if (array[i] > secondMax && array[i] != max)
+                {
+                    secondMax = array[i];
+                }
+            }
+
+            return secondMax;
+        }
+    }
+}
