25d7de89367d3b932fe235ea76c1e08653cebf2c
TheEclipsE <TheEclipsE>
2024-03-20 12:00:27 +0300

    resize array

---
+++ src/Leetcode.Test/NotLeetcode/Array/ResizeArray.cs
@@ -0,0 +1,32 @@
+﻿using FluentAssertions;
+
+namespace Leetcode.Test.NotLeetcode.Array
+{
+    public class ResizeArray
+    {
+        [Theory]
+        [InlineData(new int[] { 1, }, 2, new int[] { 1, 0 })]
+        [InlineData(new int[] { 1, 2, 3 }, 2, new int[] { 1, 2 })]
+        [InlineData(new int[] { 1, 2, 3 }, 6, new int[] { 1, 2, 3, 0, 0, 0 })]
+        public void Test(int[] array, int newSize, int[] expected)
+        {
+            Resize(ref array, newSize);
+            array.Should().Equal(expected);
+        }
+
+        private void Resize(ref int[] array, int newSize)
+        {
+            var newArray = new int[newSize];
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (i < newArray.Length)
+                {
+                    newArray[i] = array[i];
+                }
+            }
+
+            array = newArray;
+        }
+    }
+}
