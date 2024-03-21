b92842b040ba16d8780efb0c5b2d98804f2dddc0
TheEclipsE <TheEclipsE>
2024-03-21 10:05:46 +0300

    Is String Palindrome

---
+++ src/Leetcode.Test/NotLeetcode/String/IsStringPalindrome.cs
@@ -0,0 +1,36 @@
+﻿using FluentAssertions;
+
+namespace Leetcode.Test.NotLeetcode.String
+{
+    public class IsStringPalindrome
+    {
+        [Theory]
+        [InlineData("madam", true)]
+        [InlineData("kallak", true)]
+        [InlineData("kalK", false)]
+        [InlineData("k", true)]
+        [InlineData("ka", false)]
+        public void Test(string input, bool expected)
+        {
+            IsPalindrome(input).Should().Be(expected);
+        }
+
+        private bool IsPalindrome(string s)
+        {
+            int l = 0;
+            int r = s.Length - 1;
+
+            while (l < r)
+            {
+                if (s[l] != s[r])
+                {
+                    return false;
+                }
+                l++;
+                r--;
+            }
+
+            return true;
+        }
+    }
+}
