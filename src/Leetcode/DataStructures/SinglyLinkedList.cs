c05ba03345fa0ec4d1ef4fe67483d6c3c2fffe5d
TheEclipsE <TheEclipsE>
2024-03-21 20:48:45 +0300

    Singly Linked List; + print list

---
+++ src/Leetcode/DataStructures/SinglyLinkedList.cs
@@ -0,0 +1,28 @@
+﻿namespace Leetcode.DataStructures
+{
+    public class SinglyLinkedList<T>
+    {
+        public SinglyLinkedListNode<T>? Head;
+
+        public void Print()
+        {
+            var current = this.Head;
+            while (current is not null)
+            {
+                Console.WriteLine($"{current.Data}");
+                current = current.Next;
+            }
+        }
+    }
+    public class SinglyLinkedListNode<T>
+    {
+        public T Data;
+        public SinglyLinkedListNode<T>? Next;
+
+        public SinglyLinkedListNode(T data)
+        {
+            Data = data;
+            Next = null;
+        }
+    }
+}
---
+++ src/Leetcode/Program.cs
@@ -0,0 +1,30 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Leetcode.DataStructures;
+
+Console.WriteLine("Hello, World!");
+
+#region SinglyLinkedListNode
+
+#region Print
+
+{
+    SinglyLinkedList<int> linkedList = new SinglyLinkedList<int>();
+
+    linkedList.Head = new SinglyLinkedListNode<int>(0);
+
+    var fst = new SinglyLinkedListNode<int>(1);
+    var snd = new SinglyLinkedListNode<int>(2);
+    var trd = new SinglyLinkedListNode<int>(3);
+    var fth = new SinglyLinkedListNode<int>(4);
+
+    linkedList.Head.Next = fst;
+    fst.Next = snd;
+    snd.Next = trd;
+    trd.Next = fth;
+
+    linkedList.Print();
+}
+
+#endregion
+
+#endregion
