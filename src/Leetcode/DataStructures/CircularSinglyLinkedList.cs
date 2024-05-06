14c6d51deb72feb611bd998fc032ca381829f15c
TheEclipsE <TheEclipsE@tut.by>
2024-05-06 10:58:55 +0200

    CircularSinglyLinkedList

---
+++ src/Leetcode/DataStructures/CircularSinglyLinkedList.cs
@@ -0,0 +1,20 @@
+﻿namespace Leetcode.DataStructures
+{
+    public class CircularSinglyLinkedList
+    {
+        public C_Node Last;
+        public int Lenght;
+    }
+
+    public class C_Node
+    {
+        public C_Node? Next;
+        public int Val;
+
+        public C_Node(int val, C_Node next = null)
+        {
+            Val = val;
+            Next = next;
+        }
+    }
+}
