e3e7b5875602713c69707238377c71f22224d40b
TheEclipsE <TheEclipsE@tut.by>
2024-05-17 15:00:33 +0200

    Easy 509. Fibonacci Number

---
+++ src/Leetcode.Test/Leetcode/DynamicProgramming/Easy509FibonacciNumber.cs
@@ -0,0 +1,28 @@
+﻿using FluentAssertions;
+
+namespace Leetcode.Test.Leetcode.DynamicProgramming
+{
+    public class Easy509FibonacciNumber
+    {
+        [Theory]
+        [InlineData(0, 0)]
+        [InlineData(1, 1)]
+        [InlineData(2, 1)]
+        [InlineData(3, 2)]
+        [InlineData(4, 3)]
+        [InlineData(4, 3)]
+        [InlineData(10, 55)]
+        public void Test(int n, int expected)
+        {
+            Fib(n).Should().Be(expected);
+        }
+
+        public int Fib(int n)
+        {
+            if (n == 0) return 0;
+            if (n == 1) return 1;
+
+            return Fib(n - 1) + Fib(n - 2);
+        }
+    }
+}
