5bb0fddb89ef32d446ec57c620a9feb1934ec858
TheEclipsE <TheEclipsE@tut.by>
2024-05-23 09:52:11 +0200

    Easy 520. Detect Capital - slow?

---
+++ src/Leetcode.Test/Leetcode/String/Easy520DetectCapital.cs
@@ -0,0 +1,47 @@
+﻿using FluentAssertions;
+
+namespace Leetcode.Test.Leetcode.String
+{
+    public class Easy520DetectCapital
+    {
+        [Theory]
+        [InlineData("USA", true)]
+        [InlineData("leetcode", true)]
+        [InlineData("Google", true)]
+        [InlineData("RoLe", false)]
+        [InlineData("polE", false)]
+        public void Test(string input, bool expected)
+        {
+            DetectCapitalUse(input).Should().Be(expected);
+        }
+
+        public bool DetectCapitalUse(string word)
+        {
+            var capitalCount = 0;
+            foreach (var c in word)
+            {
+                if (Char.IsUpper(c))
+                {
+                    capitalCount++;
+                }
+            }
+
+            if (capitalCount == word.Length)
+            {
+                return true;
+            }
+            if (capitalCount == 0)
+            {
+                return true;
+            }
+            if (capitalCount == 1)
+            {
+                if (Char.IsUpper(word[0]))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+}
