28e48817103171e07646f8e02f9dee6edca36972
TheEclipsE <TheEclipsE@tut.by>
2024-08-04 15:48:09 +0200

    Easy 1071.Greatest Common Divisor of Strings - UNSOLVED

---
+++ src/Leetcode.Test/Leetcode/String/Easy1071GreatestCommonDivisorofStrings.cs
@@ -0,0 +1,23 @@
+﻿using FluentAssertions;
+
+namespace Leetcode.Test.Leetcode.String
+{
+    public class Easy1071GreatestCommonDivisorofStrings
+    {
+        [Theory]
+        [InlineData("ABCABC", "ABC", "ABC")]
+        [InlineData("ABABAB", "ABAB", "AB")]
+        [InlineData("LEET", "CODE", "")]
+        public void Test(string str1, string str2, string expected)
+        {
+            var res = GcdOfStrings(str1, str2);
+            res.Should().Be(expected);
+        }
+
+        public string GcdOfStrings(string str1, string str2)
+        {
+
+            return "";
+        }
+    }
+}
