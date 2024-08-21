3bad9b66372f4daa616019650472e6349496baa6
TheEclipsE <TheEclipsE@tut.by>
2024-08-21 09:01:20 +0200

    Easy 557. Reverse Words in a String III

---
+++ src/Leetcode.Test/Leetcode/String/Easy557ReverseWordsinaString3.cs
@@ -0,0 +1,56 @@
+﻿using FluentAssertions;
+
+namespace Leetcode.Test.Leetcode.String
+{
+    public class Easy557ReverseWordsinaString3
+    {
+        [Theory]
+        [InlineData("Let's take LeetCode contest", "s'teL ekat edoCteeL tsetnoc")]
+        [InlineData("Mr Ding", "rM gniD")]
+        public void Test(string input, string output)
+        {
+            M(input).Should().Be(output);
+        }
+
+        private string M(string input)
+        {
+            var result = input.ToCharArray();
+
+            int left = 0;
+            int right = 0;
+            int cur = 0;
+            while (cur < result.Length)
+            {
+                if (!char.IsWhiteSpace(result[cur]))
+                {
+                    cur++;
+                    continue;
+                }
+
+                right = cur - 1;
+
+                while (left < right)
+                {
+                    (result[left], result[right]) = (result[right], result[left]);
+
+                    left++;
+                    right--;
+                }
+
+                left = cur + 1;
+                cur++;
+            }
+
+            right = cur - 1;
+            while (left < right)
+            {
+                (result[left], result[right]) = (result[right], result[left]);
+
+                left++;
+                right--;
+            }
+
+            return new string(result);
+        }
+    }
+}
